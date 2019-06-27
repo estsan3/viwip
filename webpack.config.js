0f4286443c384c11736c8a8103325165e51c5325
kevin.knezevic@vml.com <esantamarina@overactive.com>
2019-06-26 22:10:58 -0300

    upload webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  entry: './src/app/index.js',
+  output: {
+    path: __dirname + '/src/public/js',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          {
+            loader: 'file-loader',
+            options: {
+              query: {
+                name:'assets/[name].[ext]'
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              query: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                gifsicle: {
+                  interlaced: true,
+                },
+                optipng: {
+                  optimizationLevel: 7,
+                }
+              }
+            }
+          }
+        ]
+      } 
+    ]
+  }
+};
